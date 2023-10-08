6f72f5e15db14dab4b8a6939026e3e7ab83fb7ca
Antonio <tonse@protonmail.com>
2023-10-08 16:20:01 +0300

    Setup basic local server on port 5000

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    port: 5000,
+    fetch(request, server) {
+        return new Response('Hello world');
+    },
+});
+
+console.log(`Listening to server on port ${server.port}`);
